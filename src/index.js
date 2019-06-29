7af3b49c4718c30a0d303e5df5d1bae7d4380874
Andrew Dunkman <andrew.dunkman@gsa.gov>
2019-06-28 20:17:30 -0500

    Use Angular over React.
    The NCATS team is more familiar with Angular.

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'uswds/src/stylesheets/uswds.scss';
-
-ReactDOM.render(<App />, document.getElementById('root'));
